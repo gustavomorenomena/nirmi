e8f65a5a704e65f2046a91522925a2ebbc7326e8
Gustavo Moreno <gustavomorenomena@gmail.com>
2026-07-27 22:02:40 +0200

    Add fullscreen map

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,22 @@
+import "mapbox-gl/dist/mapbox-gl.css";
+import Map from "react-map-gl/mapbox";
+
+const MAPBOX_TOKEN = import.meta.env.VITE_MAPBOX_TOKEN;
+
 function App() {
-  return <p>Hello ! This is Nirmi App.</p>;
+  return (
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <Map
+        mapboxAccessToken={MAPBOX_TOKEN}
+        initialViewState={{
+          longitude: 3.8767, // Default: Montpellier / local center
+          latitude: 43.6108,
+          zoom: 13,
+        }}
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+      />
+    </div>
+  );
 }
 
 export default App;
