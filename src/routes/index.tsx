107892be1ed77f7b6e1d48cd5e52434502a4deee
Gustavo Moreno <gustavomorenomena@gmail.com>
2026-07-30 15:22:25 +0200

    remove homepage map

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,23 +1,7 @@
 import { createFileRoute } from "@tanstack/react-router";
-import "mapbox-gl/dist/mapbox-gl.css";
-import Map from "react-map-gl/mapbox";
-
-const MAPBOX_TOKEN = import.meta.env.VITE_MAPBOX_TOKEN;
 
 function RouteComponent() {
-  return (
-    <div className="w-screen h-screen relative">
-      <Map
-        mapboxAccessToken={MAPBOX_TOKEN}
-        initialViewState={{
-          longitude: 3.8767, // Default: Montpellier / local center
-          latitude: 43.6108,
-          zoom: 13,
-        }}
-        mapStyle="mapbox://styles/mapbox/streets-v9"
-      />
-    </div>
-  );
+  return <>Nirmi App</>;
 }
 
 export const Route = createFileRoute("/")({
